387937eb87e4120f898ad0f66d8ee9707895ea22
marcoselisorto <marcoselisorto@hotmail.com>
2019-07-09 10:32:21 -0600

    Instalación de las depedencias Sequelize, y mysql2; Creación de la carpeta config y el archivo proyecto.db; creación de los modelos del proyecto

---
+++ models/Proyecto.js
@@ -0,0 +1,100 @@
+//importamos de sequelize
+const Sequelize = require('sequelize');
+
+//importar la configuración de la base de datos (/config/db.js)
+const db = requiere('../config/db');
+
+//Definimos los modelos a utilizar
+
+const Usuario = db.define( 'usuario',{ 
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    
+    nombre:{
+        type:Sequelize.STRING
+    },
+
+    contrasenia:{
+        type: Sequelize.STRING(50)
+    },
+
+    nivelAcceso:{
+        type: Sequelize.SMALLINT
+    },
+
+    ultimaModificacion:{
+        type: Sequelize.DATE
+    },
+
+    correo:{
+        type: Sequelize.STRING(50)
+    },
+
+    url:{
+        type:Sequelize.STRING
+    }
+});
+
+const Comentario = db.define('comentario',{
+    id:{
+        type: Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+
+    },
+
+    contenido:{
+        type: Sequelize.STRING
+    },
+
+    url:{
+        type: Sequelize.STRING
+    },
+
+    usuario:{
+        type: Sequelize.STRING
+    },
+    
+    ultimaModificacion:{
+        type:Sequelize.DATE
+    },
+
+    publicacion:{
+        type: Sequelize.INTEGER
+    }
+
+});
+
+const Categoria = db.define( 'categoria',{
+    id:{
+        type:Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    nombre:{
+        type: Sequelize.STRING(100)
+    }
+});
+
+const Publicacion = db.define( 'categoria','publicacion',{
+    id:{
+        type:Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    categoria:{
+        type:Sequelize.INTEGER
+    }
+});
+
+// importar los modelos para realizarlos
+
+module.exports = Usuario;
+module.exports = Comentario;
+module.exports = Categoria;
+module.exports = Publicacion;
