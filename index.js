bca4150d4336b2fcb4361e020e9d8682806f324f
Gimena <gmnsanchezunicah@gmail.com>
2019-07-09 16:52:56 +0200

    Inicializar la instancia de express y el script para el servidor

---
+++ index.js
@@ -0,0 +1,8 @@
+// Importando los módulos de express
+const express = require('express');
+
+// Creando una App de express
+const app = express();
+
+// Inicializando el servidor de express en un puerto
+app.listen(9000);
