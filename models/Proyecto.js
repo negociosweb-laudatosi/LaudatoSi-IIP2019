10e6730bdb7868fdc7ff0a033cb9a14fd537d0a3
Sonia Villeda <sonia08_m@hotmail.com>
2019-07-19 00:20:53 -0600

    Implementación de generación de URL para los artículos

--- models/Proyecto.js
+++ models/Proyecto.js
@@ -4,6 +4,13 @@ const Sequelize = require('sequelize');
 //importar la configuración de la base de datos (/config/db.js)
 const db = require('../config/db.js');
 
+// Importar slug
+const slug = require('slug');
+
+// Importar shortid
+const shortid = require('shortid');
+
+
 const Publicacion = db.define( 'publicacion',{
     id:{
         type:Sequelize.INTEGER,
@@ -34,6 +41,22 @@ const Publicacion = db.define( 'publicacion',{
     ultimaModificacion:{
         type:Sequelize.STRING
     },
+}, {
+    hooks : {
+        beforeCreate(publicacion) {
+            console.log('Antes de insertar en la base de datos');
+            const url = slug(publicacion.titulo).toLowerCase();
+
+            publicacion.url = `${url}-${shortid.generate()}`;
+        },
+
+        beforeUpdate(publicacion) {
+            console.log('Antes de actualizar en la base de datos');
+            const url = slug(publicacion.nombre).toLowerCase();
+
+            publicacion.url = `${url}-${shortid.generate()}`;
+        }
+    }
 });
 
 // importar los modelos para realizarlos
